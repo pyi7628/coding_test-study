149ff659f99a2b711c245198f92522fc98caa9bb
Yeongin Park <pyi7628@gmail.com>
2021-07-15 09:36:43 +0900

    add code

--- sdsAlgorithm/알고리즘 기초/1103.cpp
+++ sdsAlgorithm/알고리즘 기초/1103.cpp
@@ -1,2 +1,18 @@
 //게임
 //dfs
+#include <cstdio>
+bool check[51][51];
+int arr[51][51];
+int n, m;
+int main()
+{
+    scanf("%d%d", &n, &m);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    return 0;
+}
