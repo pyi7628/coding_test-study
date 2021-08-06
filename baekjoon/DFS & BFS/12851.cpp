1b55505fca88060b8a7e54fcbc52b4737fcc0c97
Yeongin Park <pyi7628@gmail.com>
2021-08-06 13:11:12 +0900

    add code

--- baekjoon/DFS & BFS/12851.cpp
+++ baekjoon/DFS & BFS/12851.cpp
@@ -2,8 +2,7 @@
 #include <queue>
 using namespace std;
 int n, k;
-int cnt = 0;
-bool check[100002];
+int check[100002];
 struct POINT
 {
     int cnt;
@@ -12,36 +11,47 @@ struct POINT
 queue<POINT> q;
 void push_point(int pos, int cnt)
 {
-    if (pos >= 0 && pos <= 100000 && !check[pos])
+    if (pos >= 0 && pos <= 100000 && check[pos] >= cnt)
     {
         POINT temp;
         temp.cnt = cnt;
         temp.pos = pos;
-        check[pos] = true;
+
+        check[pos] = cnt;
         q.push(temp);
     }
 }
+int answer = 1000000;
+int answer_cnt = 0;
 int main()
 {
     scanf("%d%d", &n, &k);
+    for (int i = 0; i < 100002; i++)
+    {
+        check[i] = 1000000;
+    }
     POINT p;
     p.cnt = 0;
     p.pos = n;
-    check[n] = true;
+    check[n] = 0;
     q.push(p);
     while (!q.empty())
     {
         POINT cur = q.front();
         q.pop();
+        if (cur.cnt > answer)
+            break;
         if (cur.pos == k)
         {
-            printf("%d", cur.cnt);
-            return 0;
+            answer = cur.cnt;
+            answer_cnt++;
         }
         push_point(cur.pos * 2, cur.cnt + 1);
         push_point(cur.pos + 1, cur.cnt + 1);
         push_point(cur.pos - 1, cur.cnt + 1);
     }
+
+    printf("%d\n%d", answer, answer_cnt);
 
     return 0;
 }
--- baekjoon/DFS & BFS/13549.cpp
+++ baekjoon/DFS & BFS/13549.cpp
@@ -1,5 +1,5 @@
 #include <cstdio>
-#include <queue>
+#include <deque>
 using namespace std;
 int n, k;
 int cnt = 0;
@@ -9,8 +9,8 @@ struct POINT
     int cnt;
     int pos;
 };
-queue<POINT> q;
-void push_point(int pos, int cnt)
+deque<POINT> q;
+void push_back_point(int pos, int cnt)
 {
     if (pos >= 0 && pos <= 100000 && !check[pos])
     {
@@ -18,7 +18,18 @@ void push_point(int pos, int cnt)
         temp.cnt = cnt;
         temp.pos = pos;
         check[pos] = true;
-        q.push(temp);
+        q.push_back(temp);
+    }
+}
+void push_front_point(int pos, int cnt)
+{
+    if (pos >= 0 && pos <= 100000 && !check[pos])
+    {
+        POINT temp;
+        temp.cnt = cnt;
+        temp.pos = pos;
+        check[pos] = true;
+        q.push_front(temp);
     }
 }
 int main()
@@ -28,19 +39,19 @@ int main()
     p.cnt = 0;
     p.pos = n;
     check[n] = true;
-    q.push(p);
+    q.push_front(p);
     while (!q.empty())
     {
         POINT cur = q.front();
-        q.pop();
+        q.pop_front();
         if (cur.pos == k)
         {
             printf("%d", cur.cnt);
             return 0;
         }
-        push_point(cur.pos * 2, cur.cnt + 1);
-        push_point(cur.pos + 1, cur.cnt + 1);
-        push_point(cur.pos - 1, cur.cnt + 1);
+        push_front_point(cur.pos * 2, cur.cnt);
+        push_back_point(cur.pos + 1, cur.cnt + 1);
+        push_back_point(cur.pos - 1, cur.cnt + 1);
     }
 
     return 0;
