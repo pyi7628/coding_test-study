3fd96f01e55f6c15ba46c0a61db324d362dbe7bc
Yeongin Park <pyi7628@gmail.com>
2021-08-12 00:37:21 +0900

    add code

---
+++ baekjoon/2346.cpp
@@ -0,0 +1,49 @@
+#include <cstdio>
+#include <vector>
+using namespace std;
+bool check[1001];
+vector<int> v;
+int n;
+int main()
+{
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        int num;
+        scanf("%d", &num);
+        v.push_back(num);
+    }
+    int rotate = v[0];
+    int ptr = 0;
+    check[0] = true;
+    printf("1 ");
+
+    for (int i = 1; i < n; i++)
+    {
+        int cnt = 0;
+        while (rotate != cnt)
+        {
+            if (rotate < 0)
+            {
+                ptr--;
+                if (ptr == -1)
+                    ptr = n - 1;
+                if (!check[ptr])
+                    cnt--;
+            }
+            else if (rotate > 0)
+            {
+                ptr++;
+                if (ptr == n)
+                    ptr = 0;
+                if (!check[ptr])
+                    cnt++;
+            }
+        }
+        check[ptr] = true;
+        rotate = v[ptr];
+        printf("%d ", ptr + 1);
+    }
+
+    return 0;
+}
