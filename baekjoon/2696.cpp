528091250033981693b69f4b0892ae214f492d71
Yeongin Park <pyi7628@gmail.com>
2021-08-23 13:52:42 +0900

    add code

--- baekjoon/2696.cpp
+++ baekjoon/2696.cpp
@@ -1,14 +1,15 @@
+// sds 중앙값 복기
 #include <cstdio>
 #include <queue>
 using namespace std;
-priority_queue<int, vector<int>, greater<int> > min_pq;
-priority_queue<int> max_pq;
 int t, m, num, min_swap, max_swap;
 int main()
 {
     scanf("%d", &t);
     for (int i = 0; i < t; i++)
     {
+        priority_queue<int, vector<int>, greater<int> > min_pq;
+        priority_queue<int> max_pq;
         scanf("%d", &m);
         printf("%d\n", (m + 1) / 2);
         for (int j = 0; j < m; j++)
@@ -38,20 +39,13 @@ int main()
             if (!min_pq.empty() && !max_pq.empty() && min_pq.top() < max_pq.top())
             {
                 min_swap = min_pq.top();
-                max_swap = max_pq.top();
                 min_pq.pop();
-                max_pq.pop();
-                min_pq.push(max_swap);
                 max_pq.push(min_swap);
             }
             if (j % 2 == 0)
                 printf("%d ", min_pq.size() > max_pq.size() ? min_pq.top() : max_pq.top());
         }
         printf("\n");
-        while (!min_pq.empty())
-            min_pq.pop();
-        while (!max_pq.empty())
-            max_pq.pop();
     }
 
     return 0;
