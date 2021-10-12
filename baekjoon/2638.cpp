3efce0022f1e1915df097005ec0bbf4b58247c8a
Yeongin Park <pyi7628@gmail.com>
2021-10-12 15:25:28 +0900

    add code

---
+++ baekjoon/2638.cpp
@@ -0,0 +1,57 @@
+#include <cstdio>
+#include <vector>
+using namespace std;
+typedef pair<int, int> pii;
+int dir[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+int n, m, c_cnt = 0, t_cnt = 0;
+int map[101][101];
+int check[101][101];
+vector<pii> v;
+void dfs(int row, int col)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        if (row + dir[i][0] >= 0 && row + dir[i][0] < n && col + dir[i][1] >= 0 && col + dir[i][1] < m && check[row + dir[i][0]][col + dir[i][1]] != -1 && map[row + dir[i][0]][col + dir[i][1]] == 1)
+        {
+            check[row + dir[i][0]][col + dir[i][1]]++;
+            if (check[row + dir[i][0]][col + dir[i][1]] >= 2)
+            {
+                check[row + dir[i][0]][col + dir[i][1]] = -1;
+                v.push_back(make_pair(row + dir[i][0], col + dir[i][1]));
+                c_cnt++;
+            }
+        }
+        if (row + dir[i][0] >= 0 && row + dir[i][0] < n && col + dir[i][1] >= 0 && col + dir[i][1] < m && check[row + dir[i][0]][col + dir[i][1]] == 0 && map[row + dir[i][0]][col + dir[i][1]] == 0)
+        {
+            check[row + dir[i][0]][col + dir[i][1]] = 1;
+            dfs(row + dir[i][0], col + dir[i][1]);
+        }
+    }
+}
+int main()
+{
+    scanf("%d%d", &n, &m);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            scanf("%d", &map[i][j]);
+            if (map[i][j])
+                t_cnt++;
+        }
+    }
+    int cnt = 0;
+    while (c_cnt != t_cnt)
+    {
+        cnt++;
+        dfs(0, 0);
+        for (int i = 0; i < v.size(); i++)
+            map[v[i].first][v[i].second] = 0;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                check[i][j] = 0;
+    }
+    printf("%d", cnt);
+
+    return 0;
+}
