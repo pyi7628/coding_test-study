15c410ae8374f2fe33dcc7d73ab42e899b8c181f
Yeongin Park <pyi7628@gmail.com>
2021-11-08 18:32:28 +0900

    add code

--- test5.js
+++ test5.js
@@ -1,24 +1,24 @@
-var p1 = new Promise((res, rej) => {
-    setTimeout(() => {
-      res("a")
-    }, 1000)
+// var p1 = new Promise((res, rej) => {
+//     setTimeout(() => {
+//       res("a")
+//     }, 1000)
     
-})
-var p2 = new Promise((res, rej) => {
-    setTimeout(() => {
-      res("a")
-    }, 1000)
+// })
+// var p2 = new Promise((res, rej) => {
+//     setTimeout(() => {
+//       res("a")
+//     }, 1000)
     
-})
+// })
 
-p1.then(res => {
-   console.log("p1:",res); // "a"
-  return res;
-})
-p2.then(res => {
-    console.log("p2:",res); // "a"
-   return res;
- })
+// p1.then(res => {
+//    console.log("p1:",res); // "a"
+//   return res;
+// })
+// p2.then(res => {
+//     console.log("p2:",res); // "a"
+//    return res;
+//  })
  
 
 // console.log(result) // Promise {<fulfilled>: "a"}
@@ -26,4 +26,38 @@ p2.then(res => {
 // result.then(res => {
 //     console.log(result)
 //     console.log(res); // "a"
+// })
+
+
+var p1 = new Promise((res, rej) => {
+  setTimeout(() => {
+    res("a")
+  }, 1001)
+  
+})
+var p2 = new Promise((res, rej) => {
+  setTimeout(() => {
+    res("b")
+  }, 1000)
+  
+})
+
+async function a(){
+ const result = await p1
+ console.log("p1:",result); // "a"
+}
+
+async function b(){
+  const result = await p2
+  console.log("p2:",result); // "b"
+}
+a()
+b()
+// p1.then(res => {
+//  console.log("p1:",res); // "a"
+// return res;
+// })
+// p2.then(res => {
+//   console.log("p2:",res); // "a"
+//  return res;
 // })
