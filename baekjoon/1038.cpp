e3e490705c4598428f093d17900e84244c2a1b15
Yeongin Park <pyi7628@gmail.com>
2021-11-15 12:23:55 +0900

    add code

--- baekjoon/1038.cpp
+++ baekjoon/1038.cpp
@@ -1,9 +1,32 @@
-#include <cstdio>
+#include <iostream>
+#include <queue>
+#include <string>
 using namespace std;
-int data[11][11];
-int cnt;
+queue<string> q;
+int cnt = -1, n;
 int main()
 {
-
+    cin >> n;
+    for (int i = 0; i <= 9; i++)
+    {
+        q.push(to_string(i));
+    }
+    while (!q.empty())
+    {
+        string cur = q.front();
+        q.pop();
+        cnt++;
+        if (cnt == n)
+        {
+            cout << cur;
+            return 0;
+        }
+        for (int i = 0; i <= 8; i++)
+        {
+            if (cur[cur.length() - 1] - '0' > i)
+                q.push(cur + to_string(i));
+        }
+    }
+    cout << "-1";
     return 0;
 }
