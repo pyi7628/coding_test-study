20ecc79e4c43270a5766b103b070005a9adc494b
Yeongin Park <pyi7628@gmail.com>
2021-12-20 18:10:25 +0900

    add code

--- baekjoon/2638.cpp
+++ baekjoon/2638.cpp
@@ -6,7 +6,6 @@ int dir[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 int n, m, c_cnt = 0, t_cnt = 0;
 int map[101][101];
 int check[101][101];
-vector<pii> v;
 void dfs(int row, int col)
 {
     for (int i = 0; i < 4; i++)
@@ -17,7 +16,7 @@ void dfs(int row, int col)
             if (check[row + dir[i][0]][col + dir[i][1]] >= 2)
             {
                 check[row + dir[i][0]][col + dir[i][1]] = -1;
-                v.push_back(make_pair(row + dir[i][0], col + dir[i][1]));
+                map[row + dir[i][0]][col + dir[i][1]] = 0;
                 c_cnt++;
             }
         }
@@ -45,8 +44,6 @@ int main()
     {
         cnt++;
         dfs(0, 0);
-        for (int i = 0; i < v.size(); i++)
-            map[v[i].first][v[i].second] = 0;
         for (int i = 0; i < n; i++)
             for (int j = 0; j < m; j++)
                 check[i][j] = 0;
